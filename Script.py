e12eb5ea7607256cd866f8611a06db1ecacd4706
Witcher5566 <github77881@gmail.com>
2024-11-03 17:28:26 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/avengerdoomsday1"> 7x Support</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}\n\n 「<a href=""></a>」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
